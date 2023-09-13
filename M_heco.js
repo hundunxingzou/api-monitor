200644637414c34f139c85e1f9665b998266e210
hundunxingzou <morettikiven357@gmail.com>
2023-09-13 14:48:05 +0800

    commit

---
+++ M_heco.js
@@ -0,0 +1,243 @@
+const axios = require('axios').default
+const schedule = require('node-schedule')
+const express = require('express')
+const ChatBot = require('dingtalk-robot-sender')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+const robot = new ChatBot({
+  baseUrl: 'https://oapi.dingtalk.com/robot/send',
+  // 此处是钉钉生成的 webhook 中 ？号后面的token
+  accessToken:
+    'e91dfa0eba828185029ecc803c78c3e0e56bff16aec01429670cc4259504382c',
+  // 此处是签名
+  secret: 'SEC9a154a13637d8a167f2ba74ecf28500d73571352056ebaad29bb114c3323c18a',
+})
+
+// 定义规则
+let rule = new schedule.RecurrenceRule()
+rule.minute = [
+  0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job = schedule.scheduleJob(rule, () => {
+  axios
+    .get('https://webapi.mdex.one/api/v1/heco/repurchase/records')
+    .catch(function (error) {
+      // handle error
+
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/repurchase/records \n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
+let rule1 = new schedule.RecurrenceRule()
+rule1.minute = [
+  1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job1 = schedule.scheduleJob(rule1, () => {
+  axios
+    .get('https://webapi.mdex.one/api/v1/heco/repurchase/burns')
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response)
+    // })
+    .catch(function (error) {
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/repurchase/burns \n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
+
+// 定义规则
+let rule2 = new schedule.RecurrenceRule()
+rule2.minute = [
+  0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job2 = schedule.scheduleJob(rule2, () => {
+  axios
+    .get('https://webapi.mdex.one/api/v1/heco/boardroom/pools')
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response)
+    // })
+    .catch(function (error) {
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/boardroom/pools \n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
+let rule3 = new schedule.RecurrenceRule()
+rule3.minute = [
+  1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job3 = schedule.scheduleJob(rule3, () => {
+  axios
+    .get('https://webapi.mdex.one/api/v1/heco/miningpool/pools')
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response)
+    // })
+    .catch(function (error) {
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/miningpool/pools \n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
+let rule4 = new schedule.RecurrenceRule()
+rule4.minute = [
+  2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job4 = schedule.scheduleJob(rule4, () => {
+  axios
+    .get('https://webapi.mdex.one/api/v1/heco/miningpool/swap')
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response)
+    // })
+    .catch(function (error) {
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/miningpool/swap \n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
+let rule5 = new schedule.RecurrenceRule()
+rule5.minute = [
+  2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job5 = schedule.scheduleJob(rule5, () => {
+  axios
+    .get('https://webapi.mdex.one/api/v1/heco/statistical/charts')
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response)
+    // })
+    .catch(function (error) {
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/statistical/charts \n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
+let rule6 = new schedule.RecurrenceRule()
+rule6.minute = [
+  2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59,
+] // 每隔 10 秒执行一次
+
+// 启动任务
+let job6 = schedule.scheduleJob(rule6, () => {
+  axios
+    .get(
+      'https://webapi.mdex.one/api/v1/heco/bridge/chain/gas?dest_chainid=128'
+    )
+    // .then(function (response) {
+    //   // handle success
+    //   console.log(response)
+    // })
+    .catch(function (error) {
+      // 此处处理接收到的消息
+      /** 推送文本消息 */
+      const title = '接口错误发送'
+      const text =
+        `## 错误提交信息 @15817324178\n` +
+        `> 接口：https://webapi.mdex.one/api/v1/heco/bridge/chain/gas?dest_chainid=128\n\n` +
+        `> 错误信息：${error} \n`
+
+      const at2 = {
+        atMobiles: ['15817324178'],
+        isAtAll: false,
+      }
+      robot.markdown(title, text, at2)
+    })
+    .finally(function () {
+      // always executed
+    })
+})
---
+++ test.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const ChatBot = require('dingtalk-robot-sender')
+
+const defaultOptions = {
+  baseUrl: 'https://oapi.dingtalk.com/robot/send',
+  accessToken:
+    '12bc26e4b2e5db5068612928f6311ddb32e4c9a3146a3955a9c38570dbc44272',
+  secret: 'SEC55f86bcf3cf05ee85ec20dbc3c09af5a80738e02978276ab0bf963aee5b84ef3',
+}
+const robot = new ChatBot(defaultOptions)
+
+// 发送钉钉消息
+const textContent = {
+  msgtype: 'text',
+  text: {
+    content: '我就是我, 是不一样的烟火',
+  },
+  at: {
+    atMobiles: ['17750762315'],
+    isAtAll: false,
+  },
+}
+robot.send(textContent).then((res) => {
+  // TODO
+})
+
+const content = '我就是我, 是不一样的烟火'
+const at = {
+  atMobiles: ['17750762315'],
+  isAtAll: false,
+}
+// 快速发送文本消息
+robot.text(content, at)
+
+const link = {
+  text: '这个即将发布的新版本，创始人陈航（花名“无招”）称它为“红树林”。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是“红树林”？',
+  title: '时代的火车向前开',
+  picUrl: '',
+  messageUrl:
+    'https://mp.weixin.qq.com/s?__biz=MzA4NjMwMTA2Ng==&mid=2650316842&idx=1&sn=60da3ea2b29f1dcc43a7c8e4a7c97a16&scene=2&srcid=09189AnRJEdIiWVaKltFzNTw&from=timeline&isappinstalled=0&key=&ascene=2&uin=&devicetype=android-23&version=26031933&nettype=WIFI',
+}
+robot.link(link)
+
+const title = '杭州天气'
+const text =
+  '#### 杭州天气 @156xxxx8827\n' +
+  '> 9度，西北风1级，空气良89，相对温度73%\n\n' +
+  '> ![screenshot](http://image.jpg)\n' +
+  '> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n'
+const at2 = {
+  atMobiles: ['7750762315'],
+  isAtAll: false,
+}
+robot.markdown(title, text, at2)
+
+const card = {
+  title: '乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身',
+  text: `![screenshot](@lADOpwk3K80C0M0FoA) 
+                ### 乔布斯 20 年前想打造的苹果咖啡厅 
+                Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划`,
+  hideAvatar: '0',
+  btnOrientation: '0',
+  btns: [
+    {
+      title: '内容不错',
+      actionURL: 'https://www.dingtalk.com/',
+    },
+    {
+      title: '不感兴趣',
+      actionURL: 'https://www.dingtalk.com/',
+    },
+  ],
+}
+robot.actionCard(card)
+
+const links = [
+  {
+    title: '时代的火车向前开',
+    messageURL:
+      'https://mp.weixin.qq.com/s?__biz=MzA4NjMwMTA2Ng==&mid=2650316842&idx=1&sn=60da3ea2b29f1dcc43a7c8e4a7c97a16&scene=2&srcid=09189AnRJEdIiWVaKltFzNTw&from=timeline&isappinstalled=0&key=&ascene=2&uin=&devicetype=android-23&version=26031933&nettype=WIFI',
+    picURL: 'https://www.dingtalk.com/',
+  },
+  {
+    title: '时代的火车向前开2',
+    messageURL:
+      'https://mp.weixin.qq.com/s?__biz=MzA4NjMwMTA2Ng==&mid=2650316842&idx=1&sn=60da3ea2b29f1dcc43a7c8e4a7c97a16&scene=2&srcid=09189AnRJEdIiWVaKltFzNTw&from=timeline&isappinstalled=0&key=&ascene=2&uin=&devicetype=android-23&version=26031933&nettype=WIFI',
+    picURL: 'https://www.dingtalk.com/',
+  },
+]
+robot.feedCard(links)
